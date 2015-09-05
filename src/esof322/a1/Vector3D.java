89bd8b9e512d419f99ed966fa157a2a04646e620
meganweller21 <mweller213@gmail.com>
2015-09-05 16:10:14 -0600

    Added the methods and fixed Ashley's name because I misspelled it!! :P

--- src/esof322/a1/Vector3D.java
+++ src/esof322/a1/Vector3D.java
@@ -3,15 +3,34 @@ package esof322.a1;
 
 /**
  *Team 03
- * @author Megan Weller, Ashey Bertrand, Shiela Tupker
+ * @author Megan Weller, Ashley Bertrand, Shiela Tupker
  */
 public class Vector3D {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private double x;
+    private double y;
+    private double z;
+    
+    //constructor Megan
+    public Vector3D(double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+     }
+    
+    //Vector3D scale Megan
+    public Vector3D(double f){
+        x = x * f;
+        y = y * f;
+        z = z * f;            
     }
+            
+    //Vector3D add Ashley
+    //Vector3D subtract Shiela
+    //Vector3D negate Shiela
+    //double dot Ashley
+    //double magnitude Shiela
+    //toString Ashley
+    //implementation of equals Megan 
+    
     
 }
