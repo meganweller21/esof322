45523295c5849d38cd3a782b2ea43a2490c1fae3
meganweller21 <mweller213@gmail.com>
2015-09-05 15:23:53 -0600

    Created the project

---
+++ src/vector3d/Vector3D.java
@@ -0,0 +1,17 @@
+
+package vector3d;
+
+/**
+ *Team 03
+ * @author Megan Weller, Ashey Bertrand, Shiela Tupker
+ */
+public class Vector3D {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/vector3d/Vector3DTest.java
@@ -0,0 +1,9 @@
+package vector3d;
+
+/**
+ *  Team 03
+ * @author Megan Weller, Ashey Bertrand, Shiela Tupker
+ */
+public class Vector3DTest {
+    
+}
