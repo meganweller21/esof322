85f5da0e0449bd194ab12c3f4dfed61a01f7fd5d
astro_sheila <astro_sheila@192.168.1.100>
2015-09-18 18:33:06 -0600

    Linked BreezySwing library and added some necessary files.  Program is running.

--- src/esof322/a2/AdventureGameModelFacade.java
+++ src/esof322/a2/AdventureGameModelFacade.java
@@ -1,14 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package esof322.a2;
 
-/**
- *
- * @author Ashley Bertrand, Megan Weller, Sheila Tupker
- */
 public class AdventureGameModelFacade {
-    
+
+ // some private fields to reference current location,
+ // its description, what I'm carrying, etc.
+ //
+ // These methods and fields are left as exercises.
+
+  AdventureGameModelFacade() { // we initialize
+  }
+
+  public void goUp(){
+  }
+
+  public void goDown(){
+    }
+
+  public void goNorth(){
+    }
+      
+  public void goSouth(){
+    }
+
+  public void goEast(){
+    }
+      
+  public void goWest(){
+    }
+
+  // You need to finish these getView and getItems methods.
+  public String getView(){ 
+     return("My view");
+     }
+
+  public String getItems(){
+     return("My items");
+     }
+
+ // Surely you will need other methods to deal with
+ // picking up and dropping things.
+
 }
