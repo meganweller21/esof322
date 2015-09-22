640c02f43a970ea194dcd7a6b1a2b14c09fa9372
meganweller21 <mweller213@gmail.com>
2015-09-22 13:41:32 -0600

    Added some comments and deleted unneeded implementation.

--- src/esof322/a2/AdventureGameModelFacade.java
+++ src/esof322/a2/AdventureGameModelFacade.java
@@ -9,11 +9,14 @@ public class AdventureGameModelFacade {
  //
  // These methods and fields are left as exercises.
    
+ //initialize variables
     Player thePlayer;
     Adventure theCave;
     Room startRm;
     Room currentRm;
-         
+    int theDirection;  
+    
+    
   AdventureGameModelFacade() throws IOException { // we initialize
         thePlayer = new Player();
         theCave = new Adventure();
@@ -22,46 +25,44 @@ public class AdventureGameModelFacade {
      
   }
   
-  int theDirection;
-
+ //directional buttons 
    public void goUp(){
-       theDirection = 4;
-       thePlayer.go(theDirection);
-       currentRm = thePlayer.getLoc();
-       thePlayer.setRoom(currentRm);
-      
+       theDirection = 4;                    //Go UP has direction of 4
+       thePlayer.go(theDirection);          //moves our player in that direction
+       currentRm = thePlayer.getLoc();      //get location of current room
+       thePlayer.setRoom(currentRm);        //set player in the new room
    }
 
   public void goDown(){
-      theDirection = 5;
+      theDirection = 5;                     //Go DOWN has direction of 5
       thePlayer.go(theDirection);
       currentRm = thePlayer.getLoc();
       thePlayer.setRoom(currentRm);
     }
 
   public void goNorth(){
-      theDirection = 0;
-       thePlayer.go(theDirection);
+      theDirection = 0;                     //Go NORTH has direction of 0
+       thePlayer.go(theDirection); 
       currentRm = thePlayer.getLoc();
       thePlayer.setRoom(currentRm);
     }
       
   public void goSouth(){
-      theDirection = 1;
+      theDirection = 1;                     //Go SOUTH has direction of 1
        thePlayer.go(theDirection);
       currentRm = thePlayer.getLoc();
       thePlayer.setRoom(currentRm);
     }
 
   public void goEast(){
-      theDirection = 2;
+      theDirection = 2;                   //Go SOUTH has direction of 2
        thePlayer.go(theDirection);
       currentRm = thePlayer.getLoc();
       thePlayer.setRoom(currentRm);
     }
       
   public void goWest(){
-      theDirection = 3;
+      theDirection = 3;                 //Go SOUTH has direction of 3
        thePlayer.go(theDirection);
       currentRm = thePlayer.getLoc();
       thePlayer.setRoom(currentRm);
@@ -69,11 +70,8 @@ public class AdventureGameModelFacade {
 
   // You need to finish these getView and getItems methods.
   public String getView(){ 
-     currentRm = thePlayer.getLoc(); 
-     currentRm.getDesc();
-     thePlayer.look(); 
-     
-     return(currentRm.getDesc());
+     currentRm = thePlayer.getLoc();   //get location of room
+     return(currentRm.getDesc());      //return the description of the room
      }
 
   public String getItems(){
